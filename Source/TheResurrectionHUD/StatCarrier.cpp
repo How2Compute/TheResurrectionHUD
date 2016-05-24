ab73b79e36eb3d4ddaabd8698ed7f5a2eccc3cd9
HowToCompute <howtocompute123@gmail.com>
2016-05-24 20:31:13 +0200

    Added everything missed in built-in unreal source control and pushing up to github

---
+++ Source/TheResurrectionHUD/StatCarrier.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheResurrectionHUD.h"
+#include "StatCarrier.h"
+
+
+// Sets default values for this component's properties
+UStatCarrier::UStatCarrier()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UStatCarrier::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UStatCarrier::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
+float UStatCarrier::GetAmmountOfMoney()
+{
+	return Money;
+}
+
+float UStatCarrier::GetAmmountOfBullets()
+{
+	return AmmountOfBullets;
+}
+
+float UStatCarrier::GetMaxAmmountOfBullets()
+{
+	return MaxAmmountOfBullets;
+}
+
+float UStatCarrier::GetAmmountOfHealth()
+{
+	return Health;
+}
+
+float UStatCarrier::GetMaxAmmountOfHealth()
+{
+	return MaxHealth;
+}
+
+float UStatCarrier::GetAmmountOfEnergy()
+{
+	return Energy;
+}
+
+float UStatCarrier::GetMaxAmmountOfEnergy()
+{
+	return MaxEnergy;
+}
+
+float UStatCarrier::GetAmmountOfFood()
+{
+	return Food;
+}
+
+float UStatCarrier::GetMaxAmmountOfFood()
+{
+	return MaxFood;
+}
+
---
+++ Source/TheResurrectionHUD/StatCarrier.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "StatCarrier.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class THERESURRECTIONHUD_API UStatCarrier : public UActorComponent
+{
+	GENERATED_BODY()
+private:
+	int AmmountOfBullets = 25;
+	int MaxAmmountOfBullets = 25;
+	float Money = 0.0f;
+	int Health = 100;
+	int MaxHealth = 100;
+	int Energy = 10;
+	int MaxEnergy = 10;
+	int Food = 10;
+	int MaxFood = 10;
+public:	
+	// Sets default values for this component's properties
+	UStatCarrier();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+	UFUNCTION(BlueprintCallable, category = "Money")
+		float GetAmmountOfMoney();
+	UFUNCTION(BlueprintCallable, category = "Bullets")
+		float GetAmmountOfBullets();
+	UFUNCTION(BlueprintCallable, category = "Bullets")
+		float GetMaxAmmountOfBullets();
+	UFUNCTION(BlueprintCallable, category = "Health")
+		float GetAmmountOfHealth();
+	UFUNCTION(BlueprintCallable, category = "Health")
+		float GetMaxAmmountOfHealth();
+	UFUNCTION(BlueprintCallable, category = "Energy")
+		float GetAmmountOfEnergy();
+	UFUNCTION(BlueprintCallable, category = "Energy")
+		float GetMaxAmmountOfEnergy();
+	UFUNCTION(BlueprintCallable, category = "Food")
+		float GetAmmountOfFood();
+	UFUNCTION(BlueprintCallable, category = "Food")
+		float GetMaxAmmountOfFood();
+
+	
+};
